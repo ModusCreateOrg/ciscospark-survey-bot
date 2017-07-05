b285f6ff6eacfed3d1e50d9ea2bbb8551167558f
Patrick Smith <patrick@promptworks.com>
2017-07-05 15:24:24 -0400

    Ask a multiple choice question

--- src/handlers.js
+++ src/handlers.js
@@ -8,6 +8,15 @@ const addTextQuestion = (convo, question, surveyId, recordAnswer) => {
   })
 }
 
+const addMultipleChoiceQuestion = (convo, question, surveyId, recordAnswer) => {
+  const questionChoices = question.choices.map((choice, index) => `${index+1}. ${choice}`)
+  const questionText = [question.text, ...questionChoices].join('\n')
+  convo.addQuestion(questionText, (response, convo) => {
+    recordAnswer(surveyId, question.id, response.text)
+    convo.next()
+  })
+}
+
 const doSurvey = (bot, { roomId, personEmail, survey, recordAnswer }) => {
   bot.startConversation({ user: personEmail, channel: roomId }, (err, convo) => {
     for (const question of survey.data.questions) {
@@ -15,7 +24,10 @@ const doSurvey = (bot, { roomId, personEmail, survey, recordAnswer }) => {
       switch(question.type) {
         case 'text':
           addQuestionFn = addTextQuestion
-          break;
+          break
+        case 'multi':
+          addQuestionFn = addMultipleChoiceQuestion
+          break
       }
       addQuestionFn(convo, question, survey.id, recordAnswer)
     }
--- test/handlers/survey.test.js
+++ test/handlers/survey.test.js
@@ -43,3 +43,42 @@ test('bot gives a survey', async t => {
   t.true(recordAnswer.calledWith(survey.id, survey.data.questions[0].id, favoriteColor))
   t.true(recordAnswer.calledWith(survey.id, survey.data.questions[1].id, favoriteSport))
 })
+
+test('bot gives a survey with mulitple choice question', async t => {
+  const { bot, controller, user } = t.context
+
+  const survey = {
+    id: '143',
+    data: {
+      questions: [
+        { text: 'What is your favorite color?', type: 'text', id: 1 },
+        { text: 'Mac or PC?', type: 'multi', choices: ['Mac', 'PC'], id: 2 }
+      ]
+    }
+  }
+  const recordAnswer = sinon.stub()
+
+  controller.trigger(
+    'survey_started',
+    [bot, { survey, roomId: user.channel, personEmail: user.id, recordAnswer }]
+  )
+
+  const firstQuestion = await bot.nextResponse()
+  t.is(firstQuestion.text, 'What is your favorite color?')
+  t.is(firstQuestion.channel, user.channel)
+
+  const favoriteColor = 'blue'
+  user.says(favoriteColor)
+
+  const secondQuestion = await bot.nextResponse()
+  t.is(secondQuestion.text, 'Mac or PC?\n1. Mac\n2. PC')
+
+  const mac = '1'
+  user.says(mac)
+
+  const next = await bot.nextResponse()
+  t.is(next.text, 'Thanks for your responses!')
+
+  t.true(recordAnswer.calledWith(survey.id, survey.data.questions[0].id, favoriteColor))
+  t.true(recordAnswer.calledWith(survey.id, survey.data.questions[1].id, mac))
+})
