fa7f310dff160f0ef737f73d7b5983e1be56fb88
Patrick Smith <patrick@promptworks.com>
2017-07-05 15:00:28 -0400

    Refactor adding question to conversation
    This will make adding different types of questions easier.

--- src/handlers.js
+++ src/handlers.js
@@ -1,16 +1,23 @@
 const handleJoin = (bot, message) =>
   bot.reply(message, 'This trusty survey bot is here to help.')
 
+const addTextQuestion = (convo, question, surveyId, recordAnswer) => {
+  convo.addQuestion(question.text, (response, convo) => {
+    recordAnswer(surveyId, question.id, response.text)
+    convo.next()
+  })
+}
+
 const doSurvey = (bot, { roomId, personEmail, survey, recordAnswer }) => {
-  console.log('starting survey', survey)
   bot.startConversation({ user: personEmail, channel: roomId }, (err, convo) => {
     for (const question of survey.data.questions) {
-      if (question.type === 'text') {
-        convo.addQuestion(question.text, (response, convo) => {
-          recordAnswer(survey.id, question.id, response.text)
-          convo.next()
-        })
+      let addQuestionFn = addTextQuestion
+      switch(question.type) {
+        case 'text':
+          addQuestionFn = addTextQuestion
+          break;
       }
+      addQuestionFn(convo, question, survey.id, recordAnswer)
     }
 
     convo.addMessage('Thanks for your responses!')
