055b53dd52fdd5ab77b2391013ab390639fd3949
Mike Nicholaides <mike.nicholaides@gmail.com>
2017-07-12 13:13:54 -0400

    Convert survey show from Pug to Vue

--- src/javascripts/surveys/show.js
+++ src/javascripts/surveys/show.js
@@ -3,3 +3,28 @@ const userId = 'someId'
 socket = io('/')
 socket.on('connect', () => socket.emit('join', userId))
 socket.on('survey updated', (data) => console.log('SHOULD get this', data))
+
+const selector = '#survey'
+const survey = $(selector).data('survey')
+const responsesByQuestion = $(selector).data('responsesByQuestion')
+
+const surveyResults = new Vue({
+  el: selector,
+  data: {
+    id: survey.id,
+    survey,
+    responsesByQuestion
+  },
+  methods: {
+    questionAndResponses: function() {
+      return this.survey.data.questions.map(question =>
+        [question, this.responsesByQuestion[question.id] || []]
+      )
+    },
+    responsesFor: function(question, responses) {
+      return question.choices.map( (choice, index) =>
+        [choice, responses.filter(({response}) => response === `${index + 1}`)]
+      )
+    }
+  }
+})
