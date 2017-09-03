c4a7499633a3e45a98985eee8659b2fae935aae7
Mike Nicholaides <mike.nicholaides@gmail.com>
2017-09-03 00:50:19 -0400

    Include team name in room selection

--- src/webui/DummySparkUser.js
+++ src/webui/DummySparkUser.js
@@ -7,7 +7,8 @@ export default class {
       isLocked: false,
       lastActivity: '2017-05-31T19:38:20.886Z',
       creatorId: 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS9hOGRkZjk1Ni0yMjcxLTQ1ZjQtOTRjYi03YTdhNmQ5ZmUwY2U',
-      created: '2017-05-31T15:59:35.329Z'
+      created: '2017-05-31T15:59:35.329Z',
+      teamName: 'Team A'
     },
     {
       id: 'Y2lzY29zcGFyazovL3VzL1JPT00vNjM1Y2JlNjAtNDYwNi0xMWU3LTkwOTAtN2RlN2VkZjJmZDU3',
@@ -17,7 +18,8 @@ export default class {
       lastActivity: '2017-06-15T14:02:10.111Z',
       teamId: 'Y2lzY29zcGFyazovL3VzL1RFQU0vYzkxZTBjYTAtYWMzNi0xMWU2LTgzYjQtZTViOTE0ZTE0YjJj',
       creatorId: 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS8xZmIxMTdkMi02Y2M2LTRmN2UtOWYxYS1kMTIyYzQzZjIzMzQ',
-      created: '2017-05-31T13:38:10.899Z'
+      created: '2017-05-31T13:38:10.899Z',
+      teamName: 'Team A'
     },
     {
       id: 'Y2lzY29zcGFyazovL3VzL1JPT00vYmI0ZmFmYzAtNDBhNi0xMWU3LTliYzgtMDkwMjI1MGZiMjEy',
@@ -26,7 +28,7 @@ export default class {
       isLocked: false,
       lastActivity: '2017-05-31T15:59:44.614Z',
       creatorId: 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS8zZGMxZDAzYS1iYTNiLTQwMmYtODU1Ni00YWI1ZDhlNDViYjA',
-      created: '2017-05-24T17:30:50.940Z'
+      created: '2017-05-24T17:30:50.940Z',
     },
     {
       id: 'Y2lzY29zcGFyazovL3VzL1JPT00vYzBkYTIwZjAtMzRjMi0xMWU3LWI1NzQtNmQwM2QwYjI4MGVm',
@@ -36,7 +38,30 @@ export default class {
       lastActivity: '2017-06-09T21:36:04.118Z',
       teamId: 'Y2lzY29zcGFyazovL3VzL1RFQU0vYzkxZTBjYTAtYWMzNi0xMWU2LTgzYjQtZTViOTE0ZTE0YjJj',
       creatorId: 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS8xZmIxMTdkMi02Y2M2LTRmN2UtOWYxYS1kMTIyYzQzZjIzMzQ',
-      created: '2017-05-09T14:21:12.212Z'
+      created: '2017-05-09T14:21:12.212Z',
+      teamName: 'Team A'
+    },
+    {
+      id: 'ABC',
+      title: 'Team ABC',
+      type: 'group',
+      isLocked: false,
+      lastActivity: '2017-06-09T21:36:04.118Z',
+      teamId: 'ABC',
+      creatorId: 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS8xZmIxMTdkMi02Y2M2LTRmN2UtOWYxYS1kMTIyYzQzZjIzMzQ',
+      created: '2017-05-09T14:21:12.212Z',
+      teamName: 'Team ABC'
+    },
+    {
+      id: 'XYZ',
+      title: 'Team XYZ',
+      type: 'group',
+      isLocked: false,
+      lastActivity: '2017-06-09T21:36:04.118Z',
+      teamId: 'XYZ',
+      creatorId: 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS8xZmIxMTdkMi02Y2M2LTRmN2UtOWYxYS1kMTIyYzQzZjIzMzQ',
+      created: '2017-05-09T14:21:12.212Z',
+      teamName: undefined
     }
   ]
 }
